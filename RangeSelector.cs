2bd383eba74596289342758469ee5395de1b67a0
Duca Dumitru <dumitru_duca@outlook.com>
2021-08-25 16:19:23 +0200

    Fixed an issue with the image drawing

--- RangeSelector.cs
+++ RangeSelector.cs
@@ -151,30 +151,6 @@ namespace RangeSelect
 
         #region Events
 
-        public static Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            var destImage = new Bitmap(width, height);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
-        }
         // Paint event
         void SelectionRangeSlider_Paint(object sender, PaintEventArgs e)
         {
@@ -191,6 +167,7 @@ namespace RangeSelect
                     Rectangle source_rect = new Rectangle(0, 0, image.Width, image.Height);
                     e.Graphics.DrawImage(image, dest_rect, source_rect, GraphicsUnit.Pixel);
                 }
+                bm.Dispose();
 
             }
             else
