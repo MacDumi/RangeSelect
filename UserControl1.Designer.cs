25cc92de4d812a98059e362beb71a1c29cd681c7
Duca Dumitru <dumitru_duca@outlook.com>
2021-08-25 12:04:03 +0200

    Add project files.

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace RangeSelect
+{
+    partial class SelectionRangeSlider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelBG = new System.Windows.Forms.Panel();
+            this.labelMin = new System.Windows.Forms.Label();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.labelMax);
+            this.panel1.Controls.Add(this.labelMin);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 188);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(376, 17);
+            this.panel1.TabIndex = 2;
+            // 
+            // panelBG
+            // 
+            this.panelBG.BackColor = System.Drawing.Color.Transparent;
+            this.panelBG.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBG.Location = new System.Drawing.Point(0, 0);
+            this.panelBG.Name = "panelBG";
+            this.panelBG.Size = new System.Drawing.Size(376, 188);
+            this.panelBG.TabIndex = 3;
+            // 
+            // labelMin
+            // 
+            this.labelMin.AutoSize = true;
+            this.labelMin.Dock = System.Windows.Forms.DockStyle.Left;
+            this.labelMin.ForeColor = System.Drawing.Color.Black;
+            this.labelMin.Location = new System.Drawing.Point(0, 0);
+            this.labelMin.Name = "labelMin";
+            this.labelMin.Size = new System.Drawing.Size(35, 13);
+            this.labelMin.TabIndex = 1;
+            this.labelMin.Text = "label1";
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelMax.ForeColor = System.Drawing.Color.Black;
+            this.labelMax.Location = new System.Drawing.Point(341, 0);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(35, 13);
+            this.labelMax.TabIndex = 2;
+            this.labelMax.Text = "label1";
+            // 
+            // SelectionRangeSlider
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.panelBG);
+            this.Controls.Add(this.panel1);
+            this.Name = "SelectionRangeSlider";
+            this.Size = new System.Drawing.Size(376, 205);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelBG;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.Label labelMin;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace RangeSelect
+{
+    public partial class SelectionRangeSlider : UserControl
+    {
+        /// <summary>
+        /// Background Image.
+        /// </summary>
+        [Description("Background image of the range selector.")]
+        public Image Image
+        {
+            get { return image; }
+            set { image = value; Invalidate(); }
+        }
+        Image image = null;
+        /// <summary>
+        /// Minimum value of the slider.
+        /// </summary>
+        [Description("Minimum value of the slider.")]
+        public int Min
+        {
+            get { return min; }
+            set { min = value; Invalidate(); }
+        }
+        int min = 0;
+        /// <summary>
+        /// Maximum value of the slider.
+        /// </summary>
+        [Description("Maximum value of the slider.")]
+        public int Max
+        {
+            get { return max; }
+            set { max = value; Invalidate(); }
+        }
+        int max = 100;
+        /// <summary>
+        /// Minimum value of the selection range.
+        /// </summary>
+        [Description("Minimum value of the selection range.")]
+        public int SelectedMin
+        {
+            get { return selectedMin; }
+            set
+            {
+                selectedMin = value;
+                if (SelectionChanged != null)
+                    SelectionChanged(this, null);
+                Invalidate();
+            }
+        }
+        int selectedMin = 0;
+        /// <summary>
+        /// Maximum value of the selection range.
+        /// </summary>
+        [Description("Maximum value of the selection range.")]
+        public int SelectedMax
+        {
+            get { return selectedMax; }
+            set
+            {
+                selectedMax = value;
+                if (SelectionChanged != null)
+                    SelectionChanged(this, null);
+                Invalidate();
+            }
+        }
+        int selectedMax = 100;
+        /// <summary>
+        /// Color of the background.
+        /// </summary>
+        [Description("Background color.")]
+        public Color BackgroundColor
+        {
+            get { return bgBrush.Color; }
+            set { bgBrush.Color = value; Invalidate(); }
+        }
+        SolidBrush bgBrush = new SolidBrush(Color.White);
+        /// <summary>
+        /// Color of the selected range.
+        /// </summary>
+        [Description("Selected range color.")]
+        public Color SelectionColor
+        {
+            get { return brush.Color; }
+            set { brush.Color = value; Invalidate(); }
+        }
+        SolidBrush brush = new SolidBrush(Color.FromArgb(255, Color.Green));
+        /// <summary>
+        /// Fired when the selected range changes.
+        /// </summary>
+        [Description("Fired when the selceted range changes.")]
+        public event EventHandler SelectionChanged;
+  
+
+        public SelectionRangeSlider()
+        {
+            InitializeComponent();
+            //avoid flickering
+            typeof(Panel).InvokeMember("DoubleBuffered",
+            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+             null, panelBG, new object[] { true });
+            panelBG.Paint += new PaintEventHandler(SelectionRangeSlider_Paint);
+            panelBG.MouseDown += new MouseEventHandler(SelectionRangeSlider_MouseDown);
+            panelBG.MouseMove += new MouseEventHandler(SelectionRangeSlider_MouseMove);
+
+            //change the text on labels
+            labelMin.Text = min.ToString();
+            labelMax.Text = max.ToString();
+        }
+
+        void SelectionRangeSlider_Paint(object sender, PaintEventArgs e)
+        {
+            var panel = sender as Panel;
+
+            if (image != null)
+            {
+                //draw the image
+                e.Graphics.DrawImage(image, ClientRectangle);
+            }
+            else
+            {
+                //paint background in white
+                e.Graphics.FillRectangle(bgBrush, ClientRectangle);
+            }
+            
+            //paint selection range in blue
+            Rectangle selectionRect = new Rectangle(
+                (selectedMin - Min) * panel.Width / (Max - Min),
+                0,
+                (selectedMax - selectedMin) * panel.Width / (Max - Min),
+                panel.Height);
+            e.Graphics.FillRectangle(brush, selectionRect);           
+        }
+
+        void SelectionRangeSlider_MouseDown(object sender, MouseEventArgs e)
+        {
+            //check where the user clicked so we can decide which thumb to move
+            int pointedValue = Min + e.X * (Max - Min) / Width;
+            int distMin = Math.Abs(pointedValue - SelectedMin);
+            int distMax = Math.Abs(pointedValue - SelectedMax);
+            int minDist = Math.Min(distMin, distMax);
+            if (minDist == distMin)
+                movingMode = MovingMode.MovingMin;
+            else
+                movingMode = MovingMode.MovingMax;
+            //call this to refreh the position of the selected thumb
+            SelectionRangeSlider_MouseMove(sender, e);
+        }
+
+        void SelectionRangeSlider_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if the left button is pushed, move the selected thumb
+            if (e.Button != MouseButtons.Left)
+                return;
+            int pointedValue = Min + e.X * (Max - Min) / Width;
+            if (movingMode == MovingMode.MovingMin)
+            {
+                SelectedMin = pointedValue;
+                labelMin.Text = SelectedMin.ToString();
+            }
+            else if(movingMode == MovingMode.MovingMax)
+            {
+                SelectedMax = pointedValue;
+                labelMax.Text = SelectedMax.ToString();
+            }
+
+        }
+
+        /// <summary>
+        /// Enumeration for the slider edge
+        /// </summary>
+        enum MovingMode {MovingMin, MovingMax }
+        MovingMode movingMode;
+    }
+}
